16db0d3b0092b21f818acae1fe76a7007453bc4e
Vadim Petrov <pvg.tag@yandex.ru>
2021-02-27 21:46:43 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 == 1) {
+      return false;
+  }
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+      let flag = false;
+      for (let j = 0; j < bracketsConfig.length; j++) {
+          if (str[i] == bracketsConfig[j][0]) {
+              flag = true;
+              if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
+                  if (stack[stack.length - 1] == bracketsConfig[j][0]) {
+                      stack.pop();
+                      break;
+                  }
+              }  
+              stack.push(bracketsConfig[j][1]);
+              break;
+          }
+          
+      }
+
+      if (!flag){
+        if (stack.length == 0 || str[i] != stack[stack.length - 1]) {
+            return false;
+        }
+        stack.pop();
+      }
+  }
+  return stack.length == 0;
 }
